cb61f6c649978c15f19f1f970378e5d68c97785f
Matt Larkin <mattlarkin@me.com>
2015-03-20 19:00:35 -0400

    Third commit

--- Assessment2/CityDetailViewController.h
+++ Assessment2/CityDetailViewController.h
@@ -6,6 +6,7 @@
 //  Copyright (c) 2015 Matt Larkin. All rights reserved.
 //
 
+#import "OverViewController.h"
 #import <UIKit/UIKit.h>
 #import "City.h"
 
--- Assessment2/CityDetailViewController.m
+++ Assessment2/CityDetailViewController.m
@@ -8,22 +8,17 @@
 
 #import "CityDetailViewController.h"
 #import "webViewController.h"
-#import "City.h"
 
 @interface CityDetailViewController () <UITextFieldDelegate , CityDelegate>
 
 
-@property (strong, nonatomic) IBOutlet UIImageView *imageView;
-@property (strong, nonatomic) IBOutlet UITextField *stateTextField;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UITextField *stateTextField;
 
-@property (strong, nonatomic) IBOutlet UITextField *nameTextField;
-@property (strong, nonatomic) IBOutlet UILabel *wikiLabel;
-@property (strong, nonatomic) IBOutlet UILabel *stateLabel;
-@property (strong, nonatomic) IBOutlet UILabel *nameLabel;
-
-@property BOOL isEditing;
-@property BOOL isEditButtonPressed;
-
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UILabel *wikiLabel;
+@property (weak, nonatomic) IBOutlet UILabel *stateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 
 @property NSString *url;
 
@@ -57,9 +52,17 @@
     return YES;
 }
 
+- (IBAction)unwindFromwebView:(UIStoryboardSegue *)sender
+{
+
+}
+
+
 -(IBAction)onWikiLabelTapped:(UITapGestureRecognizer *)tapGestureRecongizer
 {
     [self.city getWikipediaURL];
+
+    
 }
 
 #pragma mark - IBActions
@@ -73,49 +76,59 @@
 
 - (IBAction)onEditButtonPressed:(UIBarButtonItem *)sender {
 
-    if (self.editing) {
-        self.editing = NO;
-        self.nameLabel.hidden = NO;
-        self.stateLabel.hidden = NO;
-        self.nameTextField.hidden = YES;
-        self.stateTextField.hidden = YES;
-        self.city.cityName = self.nameTextField.text;
-        self.city.state = self.stateTextField.text;
-        self.nameLabel.text = self.city.cityName;
-        self.stateLabel.text = self.city.state;
-        sender.title = @"Edit";
-        [self saveData];
-    }
-    else {
-        self.editing = YES;
-        self.nameLabel.hidden = YES;
-        self.stateLabel.hidden = YES;
+    if ([sender.title isEqualToString:@"Edit"]) {
+        sender.title = @"Done";
         self.nameTextField.hidden = NO;
         self.stateTextField.hidden = NO;
-        self.nameTextField.text = self.city.cityName;
-        self.stateTextField.text = self.city.state;
-        sender.title = @"Done";
-
+    }else {
+        sender.title = @"Edit";
+        self.nameTextField.hidden = YES;
+        self.stateTextField.hidden = YES;
+        
         [self saveData];
     }
+}
+
+
+//    if (self.editing) {
+//        self.editing = NO;
+//        self.nameLabel.hidden = NO;
+//        self.stateLabel.hidden = NO;
+//        self.nameTextField.hidden = YES;
+//        self.stateTextField.hidden = YES;
+//        self.city.cityName = self.nameTextField.text;
+//        self.city.state = self.stateTextField.text;
+//        self.nameLabel.text = self.city.cityName;
+//        self.stateLabel.text = self.city.state;
+//        sender.title = @"Edit";
+//        [self saveData];
+//    }
+//    else {
+//        self.editing = YES;
+//        self.nameLabel.hidden = YES;
+//        self.stateLabel.hidden = YES;
+//        self.nameTextField.hidden = NO;
+//        self.stateTextField.hidden = NO;
+//        self.nameTextField.text = self.city.cityName;
+//        self.stateTextField.text = self.city.state;
+//        sender.title = @"Done";
+//
+//        [self saveData];
+//    }
 
    //self.stateTextField.hidden = !self.stateTextField.hidden;
 
 
-}
+//}
 
-- (IBAction)onTapGesture:(UITapGestureRecognizer *)gesture {
 
-    CGPoint touchPoint = [gesture locationInView:self.view];
 
-    if (CGRectContainsPoint(self.wikiLabel.frame, touchPoint)) {
-        [self performSegueWithIdentifier:@"WikiSegue" sender:self];
-    }
-}
 
 - (IBAction)onSetTitleButtonPressed:(UIButton *)sender {
     [self.delegate onSetTitlePressed:self.nameLabel.text];
 }
+
+
 
 /**
  *  WebView Segue
@@ -123,11 +136,11 @@
  *  @param segue  To Wiki View Controller
  *  @param sender Detail View Controller
  */
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    webViewController *vc = segue.destinationViewController;
-    vc.cityName = self.city;
-}
+//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+//{
+//    webViewController *vc = segue.destinationViewController;
+//    vc.cityName = self.city;
+//}
 
 /**
  *  Unwind back to previous view controller
